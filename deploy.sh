5ef5225d4b4a86969784371890fd223fb08f391c
Oliver Gruenwald <oliverg@OliverGruenwald-MacBook-Pro.local>
2018-11-09 13:03:14 +0100

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t fatalis/multi-client:latest -t fatalis/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t fatalis/multi-server:latest -t fatalis/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t fatalis/multi-worker:latest -t fatalis/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push fatalis/multi-client:latest
-docker push fatalis/multi-server:latest
-docker push fatalis/multi-worker:latest
+docker push stephengrider/multi-client:latest
+docker push stephengrider/multi-server:latest
+docker push stephengrider/multi-worker:latest
 
-docker push fatalis/multi-client:$SHA
-docker push fatalis/multi-server:$SHA
-docker push fatalis/multi-worker:$SHA
+docker push stephengrider/multi-client:$SHA
+docker push stephengrider/multi-server:$SHA
+docker push stephengrider/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=fatalis/multi-server:$SHA
-kubectl set image deployments/client-deployment client=fatalis/multi-client:$SHA
-kubectl set image deployments/worker-deplyoment worker=fatalis/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
